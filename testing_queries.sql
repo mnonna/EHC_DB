e25744b047f042de9685f93a66c153bf491781c7
mnonna <38696153+mnonna@users.noreply.github.com>
2019-07-25 21:14:56 +0200

    Update testing_queries.sql

--- testing_queries.sql
+++ testing_queries.sql
@@ -8,6 +8,8 @@ CALL procedureClientAdd('Tiger', 'Bonzo', 'tajger@bęc.com', '945378621','Przemy
 						'92130915324', 'TRA45678');
 CALL procedureClientAdd('Jan', 'Kowalski', 'jkow@gmail.com', '168596124','Kościuszki','7B/3','65-143',
 						'Sosnowiec','14250637891', 'SHW14523');
+CALL procedureClientAdd('Michał', 'Rożek', 'rozek@gmail.com', '125746986','Kościuszki','7B/3','65-143',
+						'Radom','11024563879', 'SGQ41578');
 
 
 CALL procedureSelectAllPESEL();
@@ -16,41 +18,26 @@ CALL procedureClientSelectByPESEL('87210564859');
 
 #POKOJE / REZERWACJE##
 
-#CALL procedureAddRoom(1,11,40,'Pokój standard dla jednej osoby - SGL. Zawiera: Łóżko, stolik nocny,
-#					  garderobę, lampkę nocną, wieszak ścienny na odzież wierzchnią, prysznic, telefon, TV LED 30", WI-FI',1);
-#CALL procedureAddRoom(1,12,120,'Pokój superior dla dwóch osób - DBL. Zawiera: Łóżko king size,klimatyzacja,
-#					  wanna, telefon TV LED 50", WI-FI',2);
-#CALL procedureAddRoom(1,13,90,'Pokój dwuosobowy z dwoma łóżkami - TWIN. Zawiera: Dwa łóżka (osobno), stoliki nocne,
-#					  garderoba, lampka nocna, prysznic, telefon, TV LED 40", WI-FI',2);
-#CALL procedureAddRoom(2,21,300,'Pokój deluxe dla dwóch osób. Zawiera: Łóżko king size, balkon - widok na morze,
-#					  klimatyzacja, wanna, garderoba, ekspres do kawy, bezpłatny minibar, TV LED 60", WI-FI',3);
-#CALL procedureAddRoom(2,22,120,'Pokój executive dla jednej osoby. Zawiera: Łóżko, stolik nocny,garderobę, biurko,
-#			          lampkę nocną, wieszak ścienny na odzież wierzchnią, prysznic, telefon, TV LED 40", WI-FI',1);
-#CALL procedureAddRoom(2,23,400,'Pokój rodzinny deluxe - maks.5 osób. Zawiera: Łóżko king size, balkon - widok na morze,
-#                      dwa dodatkowe łóżka (osobno), klimatyzacja, wanna, garderoba, ekspres do kawy, bezpłatny minibar,
-#                      konsola PS4, TV LED 60", WI-FI',5);
-#CALL procedureAddRoom(2,24,350,'Pokój rodzinny deluxe - maks.5 osób. Zawiera: Łóżko king size, balkon - widok na morze,
-#                      dwa dodatkowe łóżka (osobno), klimatyzacja, wanna, garderoba, ekspres do kawy, bezpłatny minibar,
-#                      konsola PS4, TV LED 60", WI-FI',5);
-#CALL procedureAddRoom(2,210,120,'Pokój executive dla jednej osoby. Zawiera: Łóżko, stolik nocny,garderobę, biurko,
-#			          lampkę nocną, wieszak ścienny na odzież wierzchnią, prysznic, telefon, TV LED 40", WI-FI',1);
-
-
-CALL procedureAddRoom(1,101,40,'SGL',1);
-CALL procedureAddRoom(1,102,120,'DBL',2);
-CALL procedureAddRoom(1,103,90,'TWIN',2);
-CALL procedureAddRoom(2,201,300,'Deluxe',3);
-CALL procedureAddRoom(2,202,120,'Executive SGL',1);
-CALL procedureAddRoom(2,203,400,'Deluxe',5);
-#CALL procedureAddRoom(2,24,350,'Deluxe',5);
-CALL procedureAddRoom(2,205,40,'SGL',1);
-CALL procedureAddRoom(2,206,90,'TWIN',2);
-CALL procedureAddRoom(2,207,120,'Executive SGL',1);
-CALL procedureAddRoom(2,208,350,'Deluxe',5);
-CALL procedureAddRoom(2,209,40,'SGL',1);
-CALL procedureAddRoom(2,210,90,'TWIN',2);
-CALL procedureAddRoom(2,211,120,'Executive SGL',1);
-CALL procedureAddRoom(2,212,120,'Executive SGL',1);
+CALL procedureAddRoom(1,101,40,'Pokój standard dla jednej osoby - SGL. Zawiera: Łóżko, stolik nocny,
+					  garderobę, lampkę nocną, wieszak ścienny na odzież wierzchnią, prysznic, telefon, TV LED 30", WI-FI',1);
+CALL procedureAddRoom(1,102,120,'Pokój superior dla dwóch osób - DBL. Zawiera: Łóżko king size,klimatyzacja,
+					  wanna, telefon TV LED 50", WI-FI',2);
+CALL procedureAddRoom(1,103,90,'Pokój dwuosobowy z dwoma łóżkami - TWIN. Zawiera: Dwa łóżka (osobno), stoliki nocne,
+					  garderoba, lampka nocna, prysznic, telefon, TV LED 40", WI-FI',2);
+CALL procedureAddRoom(2,201,300,'Pokój deluxe dla dwóch osób. Zawiera: Łóżko king size, balkon - widok na morze,
+					  klimatyzacja, wanna, garderoba, ekspres do kawy, bezpłatny minibar, TV LED 60", WI-FI',3);
+CALL procedureAddRoom(2,202,120,'Pokój executive dla jednej osoby. Zawiera: Łóżko, stolik nocny,garderobę, biurko,
+			          lampkę nocną, wieszak ścienny na odzież wierzchnią, prysznic, telefon, TV LED 40", WI-FI',1);
+CALL procedureAddRoom(2,203,400,'Pokój rodzinny deluxe - maks.5 osób. Zawiera: Łóżko king size, balkon - widok na morze,
+                      dwa dodatkowe łóżka (osobno), klimatyzacja, wanna, garderoba, ekspres do kawy, bezpłatny minibar,
+                      konsola PS4, TV LED 60", WI-FI',5);
+CALL procedureAddRoom(2,204,350,'Pokój rodzinny deluxe - maks.5 osób. Zawiera: Łóżko king size, balkon - widok na morze,
+                     dwa dodatkowe łóżka (osobno), klimatyzacja, wanna, garderoba, ekspres do kawy, bezpłatny minibar,
+                      konsola PS4, TV LED 60", WI-FI',5);
+CALL procedureAddRoom(2,210,120,'Pokój executive dla jednej osoby. Zawiera: Łóżko, stolik nocny,garderobę, biurko,
+			          lampkę nocną, wieszak ścienny na odzież wierzchnią, prysznic, telefon, TV LED 40", WI-FI',1);
+CALL procedureAddRoom(2,211,120,'Pokój executive dla jednej osoby. Zawiera: Łóżko, stolik nocny,garderobę, biurko,
+			          lampkę nocną, wieszak ścienny na odzież wierzchnią, prysznic, telefon, TV LED 40", WI-FI',1);
 
 SELECT *FROM room;
 SELECT *FROM allhotelrooms;
@@ -127,25 +114,9 @@ CALL selectAllReservations();
 
 ##UŻYTKOWNICY##
 ###################################
-INSERT INTO permissions (permissionDescription) VALUES ('ADMIN');
-INSERT INTO permissions (permissionDescription) VALUES ('RECEPTIONIST');
-INSERT INTO permissions (permissionDescription) VALUES ('CLEANING LADY');
-INSERT INTO permissions (permissionDescription) VALUES ('SERVICE MAN');
-INSERT INTO permissions (permissionDescription) VALUES ('CHEF');
-INSERT INTO permissions (permissionDescription) VALUES ('GUEST');
-
-CALL addEhcUser('easyhotelcontrol@gmail.com',1);
-
-CALL addEhcUser('marcinnonna026@gmail.com',1);
-CALL addEhcUser('lperski@ehc.pl',1);
-CALL addEhcUser('skobkowiak@ehc.pl',2);
-CALL addEhcUser('sylwia@ehc.pl',3);
-CALL addEhcUser('jadzia@ehc.pl',4);
-CALL addEhcUser('bonus@ehc.pl',5);
-CALL addEhcUser('wndwa@ehc.pl',6);
-CALL selectAllUsers();
+CALL addEhcUser('mnonna','4425#SGHJBbs$%^&*!DHI7991GHjb',1,'marcinnonna026@gmail.com');
+CALL selectUser('marcinnonna026@gmail.com');
 CALL selectUserEmails(1);
-SELECT *FROM allehcclients;
 
 ##SPRZĄTACZKA I MAJSTER##
 CALL changeCleaningState(101);
@@ -163,35 +134,23 @@ CALL procedureRoomInfoState(102);
 
 CALL procedureChooseAvailableRoomsAllFloors('2019-04-10 18:00:00','2019-06-25 10:00:00'); #ADM
 
-#AKTUALNE##
-CALL updateRoomLastCleaning(101,'sylwia@ehc.pl');
-CALL updateRoomLastService(101,'jadzia@ehc.pl');
 #####
 
 SELECT *FROM services;
 
 ## JUŻ DZIAŁA :D ##
-CALL reserveAdditionalTimeTypedService('87210564859',8,'2019-07-05 12:00:00','2019-07-05 15:00:00',3);
-CALL reserveAdditionalItemTypedService('87210564859',8,'2019-07-06 19:00:00',1,2);
-CALL procedureReservationBillUp(8);
+CALL reserveAdditionalTimeTypedService('87210564859',9,'2019-07-05 12:00:00','2019-07-05 15:00:00',3);
+CALL reserveAdditionalItemTypedService('87210564859',9,'2019-07-06 19:00:00',1,2);
+CALL procedureReservationBillUp(9);
 ##########################
 
 SELECT *FROM additionalservices;
 SELECT *FROM clientsservices;
 
-##AKTUALNE UŻYTKOWNIK EHC###
-CALL addEhcUser('bonus@ehc.cs',3);
-CALL selectUser('bonus@ehc.cs');
-CALL deleteEhcUser('bonus@ehc.cs');
-SELECT *FROM users;
 
 ##WYBÓR KLIENTA PO MAILU##
 CALL procedureClientSelectEmail('body@bgc.com');
 CALL procedureClientSelect('Body','Christ','87130215687');
 CALL procedureClientSelectByPESEL('87130215687');
 
-SELECT *FROM allusers;
-SELECT *FROM clientsandreservations;
-
 CALL procedureReturnFloorNumber();
-SELECT *FROM permissions;
